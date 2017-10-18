af0c94b78a2ce010fcfa7960b4247c0b6a858d98
georgiy <ewws@mail.ru>
2017-10-18 17:39:35 +0900

    добавил движение точек возраждение вражеский короблей

---
+++ Assets/Scripts/CircleMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMove : MonoBehaviour {
+    [SerializeField]
+    Transform center;
+    
+    float radius;
+    float alpha=0;
+    // Use this for initialization
+    void Start ()
+    {
+        radius = (center.position - transform.position).magnitude;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        alpha += Time.deltaTime;
+        transform.position = new Vector3(radius * Mathf.Cos(alpha) + center.position.x, radius * Mathf.Sin(alpha) + center.position.y);
+	}
+}
